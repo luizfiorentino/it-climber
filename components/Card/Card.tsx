304b35f7bbca69a1a1917704f89a9b148bf2719b
luizfiorentino <lgfiorentino@gmail.com>
2023-11-20 16:30:19 +0100

    Added delete application feature.

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,6 +3,7 @@ import styles from "./Card.module.css";
 import Heading from "../atoms/BigHeader/BigHeader";
 import SmallHeading from "../atoms/SmallHeader/SmallHeader";
 import RegularText from "../atoms/RegularText/RegularText";
+import axios from "axios";
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
   application: {
@@ -17,7 +18,16 @@ interface CardProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 export default function Card({ application, children, ...props }: CardProps) {
-  const { title, company, link, description, feedback } = application;
+  const { id, title, company, link, description, feedback } = application;
+  const deleteApplication = async (vacancyId: number | null) => {
+    console.log("vacancy Id?", vacancyId);
+    try {
+      await axios.delete(`/api/vacancies/`, { data: { vacancyId } });
+      console.log(`Vacancy with id ${vacancyId} deleted.`);
+    } catch (error) {
+      console.log("Error deleting vacancy:", error);
+    }
+  };
 
   return (
     <div className={styles.card} {...props}>
@@ -30,6 +40,7 @@ export default function Card({ application, children, ...props }: CardProps) {
       {description && <RegularText>Description: {description}</RegularText>}
       {feedback && <RegularText>Feedback: {feedback}</RegularText>}
       {children}
+      <button onClick={() => deleteApplication(id)}>Delete</button>
     </div>
   );
 }
--- pages/api/vacancies.ts
+++ pages/api/vacancies.ts
@@ -6,14 +6,16 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    console.log("from /api/vacancies, req.body:", req.body);
     try {
       const { title, company, link, description, feedback } = req.body;
       if (!title || !company) {
         console.log(
           "POST /api/vacancies - error adding new vacancy. Please inform title and company name"
         );
+        res.status(400).json({ success: false, error: "Bad Request" });
+        return;
       }
+
       const newVacancy = await prisma.vacancy.create({
         data: {
           title,
@@ -30,7 +32,75 @@ export default async function handler(
       console.error("Error creating vacancy:", error);
       res.status(500).json({ success: false, error: "Internal Server Error" });
     }
+  } else if (req.method === "DELETE") {
+    const { vacancyId } = req.body;
+    console.log("from /api/vacancies req.body??", vacancyId);
+    console.log(`Vacancy with id ${vacancyId} deleted.`);
+    try {
+      const vacancyToDelete = await prisma.vacancy.delete({
+        where: {
+          id: vacancyId,
+        },
+      });
+      res.status(200).json({ success: true, vacancy: vacancyToDelete });
+    } catch (error) {
+      console.error("Error deleting vacancy:", error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
   } else {
     res.status(405).json({ success: false, error: "Method Not Allowed" });
   }
 }
+
+// import prisma from "../../prisma/client";
+// import { NextApiRequest, NextApiResponse } from "next";
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "POST") {
+//     try {
+//       const { title, company, link, description, feedback } = req.body;
+//       if (!title || !company) {
+//         console.log(
+//           "POST /api/vacancies - error adding new vacancy. Please inform title and company name"
+//         );
+//       }
+//       const newVacancy = await prisma.vacancy.create({
+//         data: {
+//           title,
+//           company,
+//           link,
+//           description,
+//           feedback,
+//         },
+//       });
+//       console.log("new application submitted:", newVacancy);
+
+//       res.status(201).json({ success: true, vacancy: newVacancy });
+//     } catch (error) {
+//       console.error("Error creating vacancy:", error);
+//       res.status(500).json({ success: false, error: "Internal Server Error" });
+//     }
+//   } else {
+//     res.status(405).json({ success: false, error: "Method Not Allowed" });
+//   }
+
+//   if (req.method === "DELETE") {
+//     const id = req.body.vacancyId;
+//     console.log("from /api/vacancies req.body??", req.body);
+//     try {
+//       const vacancyToDelete = await prisma.vacancy.delete({
+//         where: {
+//           id,
+//         },
+//       });
+//     } catch (error) {
+//       console.error("Error deleting vacancy:", error);
+//       res.status(500).json({ success: false, error: "Internal Server Error" });
+//     }
+//   } else {
+//     res.status(405).json({ success: false, error: "Method Not Allowed" });
+//   }
+// }
