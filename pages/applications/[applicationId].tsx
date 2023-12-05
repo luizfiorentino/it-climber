fa1c25c78398a0b455faece8de8236c2f5907f2f
luizfiorentino <lgfiorentino@gmail.com>
2023-12-05 10:37:04 +0100

    Details page with correct types and displaying all related info for the application.

--- pages/applications/[applicationId].tsx
+++ pages/applications/[applicationId].tsx
@@ -1,15 +1,17 @@
-import { useState, useContext } from "react";
 import styles from "./detailsPage.module.css";
 import {
   GetServerSideProps,
   InferGetServerSidePropsType,
   GetServerSidePropsResult,
 } from "next";
-import axios from "axios";
 import prisma from "@/prisma/client";
-import Card from "@/components/Card/Card";
 import Heading from "@/components/atoms/BigHeader/BigHeader";
+import SmallHeading from "@/components/atoms/SmallHeader/SmallHeader";
 
+type Tag = {
+  id: number;
+  name: string;
+};
 type Application = {
   id: number;
   title: string;
@@ -21,26 +23,61 @@ type Application = {
   location?: string;
   language?: string;
   applicationDate?: string;
+  tags?: Tag[];
 };
 
 export default function DetailsPage({
   application,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   console.log("details page, application", application);
+
+  if (!application) {
+    return (
+      <main className={styles.main}>
+        <div>
+          <SmallHeading>Application not found</SmallHeading>
+        </div>
+      </main>
+    );
+  }
   return (
     <main className={styles.main}>
       <div>
-        <Heading>Details Page</Heading>
+        <Heading>{application.title}</Heading>
+        <SmallHeading>
+          {application.applicationDate && application.applicationDate}
+        </SmallHeading>
+        <SmallHeading>@ {application.company}</SmallHeading>
+        <SmallHeading>{application.link && application.link}</SmallHeading>
+        <SmallHeading>
+          {application.location && application.location}
+        </SmallHeading>
+        {application.tags && <SmallHeading>tags:</SmallHeading>}
+        {application.tags &&
+          application.tags.map((tag) => (
+            <SmallHeading key={tag.id}>- {tag.name}</SmallHeading>
+          ))}
+        <SmallHeading>{application.description}</SmallHeading>
+
+        <SmallHeading>
+          {application.recruiter && application.recruiter}
+        </SmallHeading>
+        <SmallHeading>
+          {application.language && `language(s): ${application.language}`}
+        </SmallHeading>
+        <SmallHeading>
+          {application.feedback && application.feedback}
+        </SmallHeading>
       </div>
     </main>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<{
-  application: Application;
+  application: Application | null;
 }> = async (
   context
-): Promise<GetServerSidePropsResult<{ response: Application }>> => {
+): Promise<GetServerSidePropsResult<{ application: Application | null }>> => {
   try {
     const applicationId = context.params?.applicationId as string;
     const application = await prisma.vacancy.findUnique({
@@ -54,7 +91,7 @@ export const getServerSideProps: GetServerSideProps<{
 
     return {
       props: {
-        application: application,
+        application: application as Application | null,
       },
     };
   } catch (error) {
